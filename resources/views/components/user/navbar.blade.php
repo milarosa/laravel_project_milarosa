188e6e719767684187eac2d63e88b285557b54ee
Mila Rosa <mila.rosa@eydendigital.co.id>
2025-12-25 20:02:46 +0700

    Add login multi user

--- resources/views/components/user/navbar.blade.php
+++ resources/views/components/user/navbar.blade.php
@@ -1,18 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container">
-        <a class="navbar-brand fw-bold text-primary" href="#">TokoKu</a>
+        <a class="navbar-brand fw-bold fs-2 text-primary" href="/">TokoKu</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-
             target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="#produk">Produk</a></li>
-                <li class="nav-item"><a class="nav-link" href="#pesanan">Pesanan</a></li>
-                <li class="nav-item"><a class="nav-link" href="#pembayaran">Pembayaran</a></li>
+                @auth
+                <li class="nav-item"><a class="nav-link" href="{{route('cart.index')}}">Menu Checkout</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{route('orders.history')}}">Daftar Pesanan</a></li>
+                @endauth
             </ul>
-            <a href="/login" class="btn btn-primary ms-lg-3">Login</a>
+            @guest
+            <a href="{{ route('login') }}" class="btn btn-primary ms-lg-3">Login</a>
+            @endguest
+            @auth
+            <form method="POST" action="{{ route('logout') }}" class="d-inline">
+                @csrf
+                <button type="submit" class="btn btn-danger ms-lg-3">Logout</button>
+            </form>
+            @endauth
         </div>
     </div>
 </nav>
